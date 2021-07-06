42796278ffaeeea5c0f3bac0f54d311013df58ef
RyoungGu Kwon <rkay8496@gmail.com>
2021-07-06 18:00:05 +0900

    complete

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import loadable from "@loadable/component";
+const SplitMe = loadable(() => import("./SplitMe"), {
+  fallback: <div>loading...</div>,
+});
 
-function App() {
+const App = () => {
+  const [visible, setVisible] = useState(false);
+  const onClick = () => {
+    setVisible(true);
+  };
+  const onMouseOver = () => {
+    SplitMe.preload();
+  };
   return (
     <div className="App">
       <header className="App-header">
@@ -14,12 +26,15 @@ function App() {
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
+          onClick={onClick}
+          onMouseOver={onMouseOver}
         >
-          Learn React
+          Hello React!
         </a>
+        {visible && <SplitMe></SplitMe>}
       </header>
     </div>
   );
-}
+};
 
 export default App;
